77f9ed01e89114b7b56428e1bb999b2de92bf247
Shoaib <shoaib.rehman@yl-solutions.de>
2024-09-27 11:45:54 +0500

    example.env file added

--- routes/xTrendsRoutes.js
+++ routes/xTrendsRoutes.js
@@ -2,8 +2,6 @@ import express from 'express';
 import { loadXtrends } from '../controllers/xTrendsController.js';
 
 const router = express.Router()
-console.log('test');
-
 router.route('/load-all-tweets').get(loadXtrends)
 
 
